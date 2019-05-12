ca492d865802dbc898b4b7663c1d64631431845f
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2019-05-12 08:18:25 -0400

    rendering the pointclound

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,10 +42,17 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
+    //
+    Lidar* lidar = new Lidar(cars, 0.0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar -> scan();
+
+    //renderRays(viewer, lidar->position, inputCloud);
+    renderPointCloud(viewer, inputCloud, "inputCloud");
+
 
     // TODO:: Create point processor
   
