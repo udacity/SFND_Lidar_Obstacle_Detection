69662b0136be27dd5899d511bf90a53f65d491ba
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2019-05-13 21:44:59 -0400

    setting up Euclidean Clustering

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -7,41 +7,82 @@
 // Structure to represent node of kd tree
 struct Node
 {
-	std::vector<float> point;
-	int id;
-	Node* left;
-	Node* right;
+  std::vector<float> point;
+  int id;
+  Node* left;
+  Node* right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+  Node(std::vector<float> arr, int setId)
+    :	point(arr), id(setId), left(NULL), right(NULL)
+  {}
 };
 
 struct KdTree
 {
-	Node* root;
+  Node* root;
 
-	KdTree()
-	: root(NULL)
-	{}
+  KdTree()
+    : root(NULL)
+  {}
 
-	void insert(std::vector<float> point, int id)
-	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+  void insertHelper(Node *&node, uint depth, std::vector<float> point, int id)
+  {
 
-	}
+    if(node == NULL)
+      node = new Node(point, id);
+    else
+    {
 
-	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
-		std::vector<int> ids;
-		return ids;
-	}
-	
+      uint cd = depth %2;
+
+      if(point[cd] < node->point[cd])
+        insertHelper((node->left), depth + 1, point, id );
+
+      else
+        insertHelper((node->right), depth + 1, point, id );
+
+    }
+  }
+
+  void insert(std::vector<float> point, int id)
+  {
+    insertHelper(root,0,point,id);
+
+  }
+
+  void searchHelper(std::vector<float> pivot, Node *&node, uint depth, float distanceTol, std::vector<int> &ids)
+  {
+    if(node != NULL)
+    {
+      if( (node->point[0] >= (pivot[0] - distanceTol)) &&
+          (node->point[0] <= (pivot[0] + distanceTol)) &&
+          (node->point[1] >= (pivot[1] - distanceTol)) &&
+          (node->point[1] <= (pivot[1] + distanceTol)) 
+         )
+      {
+
+        float distance = sqrt( pow((node->point[0] - pivot[0]),2) + pow((node->point[1] - pivot[1]),2));
+        if(distance <= distanceTol)
+          ids.push_back(node->id);
+      }
+
+
+      uint cd = depth %2;
+      if((pivot[cd] - distanceTol) < node->point[cd])
+        searchHelper(pivot,(node->left ),depth + 1, distanceTol, ids);
+      if((pivot[cd] + distanceTol) > node->point[cd])
+        searchHelper(pivot,(node->right ),depth + 1, distanceTol, ids);
+    }
+
+  }
+
+
+  std::vector<int> search(std::vector<float> pivot, float distanceTol)
+  {
+    std::vector<int> ids;
+
+    searchHelper(pivot,root,0, distanceTol, ids);
+    return ids;
+  }
 
 };
-
-
-
-
