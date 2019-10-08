b0db95129e0c4cfd8e739a4c74800b9605cc2094
YeongsooKim <yeongsookim768@konkuk.ac.kr>
2019-10-08 10:55:57 +0900

    update quiz

---
+++ src/quiz/cluster/build/CMakeFiles/quizCluster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quizCluster.dir/cluster.cpp.o"
+  "CMakeFiles/quizCluster.dir/home/kys768/workspace/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o"
+  "quizCluster.pdb"
+  "quizCluster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quizCluster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
