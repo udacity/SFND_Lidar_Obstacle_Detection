65b4250c8c2a5c027935f857126b78df305297d5
bkch <nthdimensionalreality@gmail.com>
2020-03-21 21:03:24 +0100

    more

--- src/render/render.h
+++ src/render/render.h
@@ -17,7 +17,8 @@ struct Color
 
 	Color(float setR, float setG, float setB)
 		: r(setR), g(setG), b(setB)
-	{}
+	{
+	}
 };
 
 struct Vect3
@@ -27,67 +28,71 @@ struct Vect3
 
 	Vect3(double setX, double setY, double setZ)
 		: x(setX), y(setY), z(setZ)
-	{}
-
-	Vect3 operator+(const Vect3& vec)
 	{
-		Vect3 result(x+vec.x,y+vec.y,z+vec.z);
+	}
+
+	Vect3 operator+(const Vect3 &vec)
+	{
+		Vect3 result(x + vec.x, y + vec.y, z + vec.z);
 		return result;
 	}
 };
 
 enum CameraAngle
 {
-	XY, TopDown, Side, FPS
+	XY,
+	TopDown,
+	Side,
+	FPS
 };
 
 struct Car
 {
 
 	// units in meters
-  	Vect3 position, dimensions;
-  	
-  	std::string name;
-  	Color color;
+	Vect3 position, dimensions;
 
-  	Car(Vect3 setPosition, Vect3 setDimensions, Color setColor, std::string setName)
-    	: position(setPosition), dimensions(setDimensions), color(setColor), name(setName)
-  	{}
+	std::string name;
+	Color color;
 
-  	void render(pcl::visualization::PCLVisualizer::Ptr& viewer)
+	Car(Vect3 setPosition, Vect3 setDimensions, Color setColor, std::string setName)
+		: position(setPosition), dimensions(setDimensions), color(setColor), name(setName)
+	{
+	}
+
+	void render(pcl::visualization::PCLVisualizer::Ptr &viewer)
 	{
 		// render bottom of car
-		viewer->addCube(position.x-dimensions.x/2, position.x+dimensions.x/2, position.y-dimensions.y/2, position.y+dimensions.y/2, position.z, position.z+dimensions.z*2/3, color.r, color.g, color.b, name); 
-      	viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE, name); 
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, color.r, color.g, color.b, name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, name);
+		viewer->addCube(position.x - dimensions.x / 2, position.x + dimensions.x / 2, position.y - dimensions.y / 2, position.y + dimensions.y / 2, position.z, position.z + dimensions.z * 2 / 3, color.r, color.g, color.b, name);
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE, name);
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, color.r, color.g, color.b, name);
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, name);
 		// render top of car
-		viewer->addCube(position.x-dimensions.x/4, position.x+dimensions.x/4, position.y-dimensions.y/2, position.y+dimensions.y/2, position.z+dimensions.z*2/3, position.z+dimensions.z, color.r, color.g, color.b, name+"Top"); 
-      	viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE, name+"Top"); 
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, color.r, color.g, color.b, name+"Top");
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, name+"Top");
+		viewer->addCube(position.x - dimensions.x / 4, position.x + dimensions.x / 4, position.y - dimensions.y / 2, position.y + dimensions.y / 2, position.z + dimensions.z * 2 / 3, position.z + dimensions.z, color.r, color.g, color.b, name + "Top");
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE, name + "Top");
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, color.r, color.g, color.b, name + "Top");
+		viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, name + "Top");
 	}
 
 	// collision helper function
 	bool inbetween(double point, double center, double range)
 	{
-		return (center-range <= point) && (center+range >= point);
+		return (center - range <= point) && (center + range >= point);
 	}
 
 	bool checkCollision(Vect3 point)
 	{
-		return (inbetween(point.x,position.x,dimensions.x/2)&&inbetween(point.y,position.y,dimensions.y/2)&&inbetween(point.z,position.z+dimensions.z/3,dimensions.z/3))||
-			   (inbetween(point.x,position.x,dimensions.x/4)&&inbetween(point.y,position.y,dimensions.y/2)&&inbetween(point.z,position.z+dimensions.z*5/6,dimensions.z/6));
-
+		return (inbetween(point.x, position.x, dimensions.x / 2) && inbetween(point.y, position.y, dimensions.y / 2) && inbetween(point.z, position.z + dimensions.z / 3, dimensions.z / 3)) ||
+			   (inbetween(point.x, position.x, dimensions.x / 4) && inbetween(point.y, position.y, dimensions.y / 2) && inbetween(point.z, position.z + dimensions.z * 5 / 6, dimensions.z / 6));
 	}
 };
 
-void renderHighway(pcl::visualization::PCLVisualizer::Ptr& viewer);
-void renderRays(pcl::visualization::PCLVisualizer::Ptr& viewer, const Vect3& origin, const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud);
-void clearRays(pcl::visualization::PCLVisualizer::Ptr& viewer);
-void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer, const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, std::string name, Color color = Color(1,1,1));
-void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer, const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud, std::string name, Color color = Color(-1,-1,-1));
-void renderBox(pcl::visualization::PCLVisualizer::Ptr& viewer, Box box, int id, Color color = Color(1,0,0), float opacity=1);
-void renderBox(pcl::visualization::PCLVisualizer::Ptr& viewer, BoxQ box, int id, Color color = Color(1,0,0), float opacity=1);
+void renderHighway(pcl::visualization::PCLVisualizer::Ptr &viewer);
+void renderRays(pcl::visualization::PCLVisualizer::Ptr &viewer, const Vect3 &origin, const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud);
+void clearRays(pcl::visualization::PCLVisualizer::Ptr &viewer);
+void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr &viewer, const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, std::string name, Color color = Color(1, 1, 1));
+void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr &viewer, const pcl::PointCloud<pcl::PointXYZI>::Ptr &cloud, std::string name, Color color = Color(-1, -1, -1));
+void renderBox(pcl::visualization::PCLVisualizer::Ptr &viewer, Box box, int id, Color color = Color(1, 0, 0), float opacity = 1);
+void renderBox(pcl::visualization::PCLVisualizer::Ptr &viewer, BoxQ box, int id, Color color = Color(1, 0, 0), float opacity = 1);
 
 #endif
--- src/sensors/lidar.h
+++ src/sensors/lidar.h
@@ -8,7 +8,7 @@ const double pi = 3.1415;
 
 struct Ray
 {
-	
+
 	Vect3 origin;
 	double resolution;
 	Vect3 direction;
@@ -18,16 +18,17 @@ struct Ray
 	// parameters:
 	// setOrigin: the starting position from where the ray is cast
 	// horizontalAngle: the angle of direction the ray travels on the xy plane
-	// verticalAngle: the angle of direction between xy plane and ray 
+	// verticalAngle: the angle of direction between xy plane and ray
 	// 				  for example 0 radians is along xy plane and pi/2 radians is stright up
 	// resoultion: the magnitude of the ray's step, used for ray casting, the smaller the more accurate but the more expensive
 
 	Ray(Vect3 setOrigin, double horizontalAngle, double verticalAngle, double setResolution)
-		: origin(setOrigin), resolution(setResolution), direction(resolution*cos(verticalAngle)*cos(horizontalAngle), resolution*cos(verticalAngle)*sin(horizontalAngle),resolution*sin(verticalAngle)),
+		: origin(setOrigin), resolution(setResolution), direction(resolution * cos(verticalAngle) * cos(horizontalAngle), resolution * cos(verticalAngle) * sin(horizontalAngle), resolution * sin(verticalAngle)),
 		  castPosition(origin), castDistance(0)
-	{}
+	{
+	}
 
-	void rayCast(const std::vector<Car>& cars, double minDistance, double maxDistance, pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, double slopeAngle, double sderr)
+	void rayCast(const std::vector<Car> &cars, double minDistance, double maxDistance, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, double slopeAngle, double sderr)
 	{
 		// reset ray
 		castPosition = origin;
@@ -35,7 +36,7 @@ struct Ray
 
 		bool collision = false;
 
-		while(!collision && castDistance < maxDistance)
+		while (!collision && castDistance < maxDistance)
 		{
 
 			castPosition = castPosition + direction;
@@ -45,28 +46,26 @@ struct Ray
 			collision = (castPosition.z <= castPosition.x * tan(slopeAngle));
 
 			// check if there is any collisions with cars
-			if(!collision && castDistance < maxDistance)
+			if (!collision && castDistance < maxDistance)
 			{
-				for(Car car : cars)
+				for (Car car : cars)
 				{
 					collision |= car.checkCollision(castPosition);
-					if(collision)
+					if (collision)
 						break;
 				}
 			}
 		}
 
-		if((castDistance >= minDistance)&&(castDistance<=maxDistance))
+		if ((castDistance >= minDistance) && (castDistance <= maxDistance))
 		{
 			// add noise based on standard deviation error
-			double rx = ((double) rand() / (RAND_MAX));
-			double ry = ((double) rand() / (RAND_MAX));
-			double rz = ((double) rand() / (RAND_MAX));
-			cloud->points.push_back(pcl::PointXYZ(castPosition.x+rx*sderr, castPosition.y+ry*sderr, castPosition.z+rz*sderr));
+			double rx = ((double)rand() / (RAND_MAX));
+			double ry = ((double)rand() / (RAND_MAX));
+			double rz = ((double)rand() / (RAND_MAX));
+			cloud->points.push_back(pcl::PointXYZ(castPosition.x + rx * sderr, castPosition.y + ry * sderr, castPosition.z + rz * sderr));
 		}
-			
 	}
-
 };
 
 struct Lidar
@@ -83,7 +82,7 @@ struct Lidar
 	double sderr;
 
 	Lidar(std::vector<Car> setCars, double setGroundSlope)
-		: cloud(new pcl::PointCloud<pcl::PointXYZ>()), position(0,0,2.6)
+		: cloud(new pcl::PointCloud<pcl::PointXYZ>()), position(0, 0, 2.6)
 	{
 		// TODO:: set minDistance to 5 to remove points from roof of ego car
 		minDistance = 5;
@@ -95,20 +94,20 @@ struct Lidar
 		groundSlope = setGroundSlope;
 
 		// TODO:: increase number of layers to 8 to get higher resoultion pcd
-		int numLayers = 8;  // 3
+		int numLayers = 8; // 3
 		// the steepest vertical angle
-		double steepestAngle =  30.0*(-pi/180);
-		double angleRange = 26.0*(pi/180);
+		double steepestAngle = 30.0 * (-pi / 180);
+		double angleRange = 26.0 * (pi / 180);
 		// TODO:: set to pi/64 to get higher resoultion pcd
-		double horizontalAngleInc = pi/64;	// pi/6
+		double horizontalAngleInc = pi / 64; // pi/6
 
-		double angleIncrement = angleRange/numLayers;
+		double angleIncrement = angleRange / numLayers;
 
-		for(double angleVertical = steepestAngle; angleVertical < steepestAngle+angleRange; angleVertical+=angleIncrement)
+		for (double angleVertical = steepestAngle; angleVertical < steepestAngle + angleRange; angleVertical += angleIncrement)
 		{
-			for(double angle = 0; angle <= 2*pi; angle+=horizontalAngleInc)
+			for (double angle = 0; angle <= 2 * pi; angle += horizontalAngleInc)
 			{
-				Ray ray(position,angle,angleVertical,resoultion);
+				Ray ray(position, angle, angleVertical, resoultion);
 				rays.push_back(ray);
 			}
 		}
@@ -123,7 +122,7 @@ struct Lidar
 	{
 		cloud->points.clear();
 		auto startTime = std::chrono::steady_clock::now();
-		for(Ray ray : rays)
+		for (Ray ray : rays)
 			ray.rayCast(cars, minDistance, maxDistance, cloud, groundSlope, sderr);
 		auto endTime = std::chrono::steady_clock::now();
 		auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
@@ -132,7 +131,6 @@ struct Lidar
 		cloud->height = 1; // one dimensional unorganized point cloud dataset
 		return cloud;
 	}
-
 };
 
 #endif
