46303042bef847d0f0835c04190ac1e6343ee87b
bkch <nthdimensionalreality@gmail.com>
2020-03-27 21:48:53 +0100

    optimized insert function

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -25,68 +25,26 @@ struct KdTree
 	{
 	}
 
+	void insertHelper(Node **node, uint depth, std::vector<float> point, int id)
+	{
+		// Tree is empty
+		if (*node == NULL)
+			*node = new Node(point, id);
+		else
+		{
+			// calculate the current dimension.
+			uint cd = depth % 2;
+
+			if (point[cd] < (*node)->point[cd])
+				insertHelper(&((*node)->left), depth + 1, point, id);
+			else
+				insertHelper(&((*node)->right), depth + 1, point, id);
+		}
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root
-		if (root == NULL)
-		{
-			root = new Node(point, id);
-			return;
-		}
-
-		// traverse and determine where to insert. And keep track of depth
-		unsigned depth = 0;
-		Node *currentNode = root;
-
-		while (true)
-		{
-			// compare x
-			if (depth % 2 == 0)
-			{
-				if (point[0] < currentNode->point[0])
-				{
-					if (currentNode->left == NULL)
-					{
-						currentNode->left = new Node(point, id);
-						break;
-					}
-					currentNode = currentNode->left;
-				}
-				else
-				{
-					if (currentNode->right == NULL)
-					{
-						currentNode->right = new Node(point, id);
-						break;
-					}
-					currentNode = currentNode->right;
-				}
-			}
-			// compare y
-			else
-			{
-				if (point[1] < currentNode->point[1])
-				{
-					if (currentNode->left == NULL)
-					{
-						currentNode->left = new Node(point, id);
-						break;
-					}
-					currentNode = currentNode->left;
-				}
-				else
-				{
-					if (currentNode->right == NULL)
-					{
-						currentNode->right = new Node(point, id);
-						break;
-					}
-					currentNode = currentNode->right;
-				}
-			}
-			depth++;
-		}
+		insertHelper(&root, 0, point, id);
 	}
 
 	// return a list of point ids in the tree that are within distance of target
