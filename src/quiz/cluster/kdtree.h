aa25ae566c0495ca619a893ac3f1e4e19716051a
bkch <nthdimensionalreality@gmail.com>
2020-03-30 11:34:57 +0200

    Added kdTree search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -2,6 +2,7 @@
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
+#include "math.h"
 
 // Structure to represent node of kd tree
 struct Node
@@ -47,10 +48,32 @@ struct KdTree
 		insertHelper(&root, 0, point, id);
 	}
 
+	void searchHelper(std::vector<float> target, Node *node, int depth, float distanceTol, std::vector<int> &ids)
+	{
+		if (node != NULL)
+		{
+			// perform cheap comparision
+			if ((node->point[0] >= (target[0] - distanceTol) && node->point[0] <= (target[0] + distanceTol)) &&
+				(node->point[1] >= (target[1] - distanceTol) && node->point[1] <= (target[1] + distanceTol)))
+			{
+				// Perform second round investigation
+				float distance = std::sqrt(std::pow(node->point[0] - target[0], 2) + std::pow(node->point[1] - target[1], 2));
+				if (distance <= distanceTol)
+					ids.push_back(node->id);
+			}
+			// check across boundary
+			if ((target[depth % 2] - distanceTol) < node->point[depth % 2])
+				searchHelper(target, node->left, depth + 1, distanceTol, ids);
+			if ((target[depth % 2] + distanceTol) > node->point[depth % 2])
+				searchHelper(target, node->right, depth + 1, distanceTol, ids);
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchHelper(target, root, 0, distanceTol, ids);
 		return ids;
 	}
 };
